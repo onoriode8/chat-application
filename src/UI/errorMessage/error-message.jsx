2c228c2d2fd8fe244fbfce04ffae266dab68ea02
Onoriode <onoriodeumukoro8@gmail.com>
2024-01-26 09:16:14 +0100

    update userprofile route

--- src/UI/errorMessage/error-message.jsx
+++ src/UI/errorMessage/error-message.jsx
@@ -1,17 +1,14 @@
 import PropTypes from 'prop-types'
 
 import Button from "../button/button";
-import { Backdrop } from '../../components/layout/toolbar/side-drawer/backdrop/backdrop';
+// import { Backdrop } from '../../components/layout/toolbar/side-drawer/backdrop/backdrop';
 
 import errorMessageCss from './error-message.module.css'; 
 
 const errorMessage = ({ errorMessage, clear }) => {
     return (
         <>
-        {/* <div className={errorMessageCss.backdrop}></div> */}
-        <div>
-            <Backdrop />
-        </div>
+        <div className={errorMessageCss.backdrop}></div>
         <div className={errorMessageCss.container}>
             <p>{errorMessage}</p>
             <div className={errorMessageCss.wrap}>
